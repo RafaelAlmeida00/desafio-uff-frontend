3ca178739f9a88c23256cffc80856738ee32810c
RafaelAlmeida00 <rafaelalmeid00@gmail.com>
2026-02-16 03:03:19 -0300

    fix(api): use current host as default backend url

--- src/services/api/client.ts
+++ src/services/api/client.ts
@@ -1,6 +1,11 @@
 import axios from 'axios'
 
-const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:3000'
+const browserDefaultBaseUrl =
+  typeof window !== 'undefined'
+    ? `${window.location.protocol}//${window.location.hostname}:3000`
+    : 'http://localhost:3000'
+
+const baseURL = import.meta.env.VITE_API_URL || browserDefaultBaseUrl
 
 export const apiClient = axios.create({
   baseURL,
